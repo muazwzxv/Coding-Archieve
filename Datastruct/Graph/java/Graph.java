38467bfa0daabe06271da3aa6553e7730bd18bb3
Muaz Wazir <muazwazir@gmail.com>
2021-02-15 15:08:09 +0800

    dfs traversal

--- Datastruct/Graph/java/Graph.java
+++ Datastruct/Graph/java/Graph.java
@@ -79,4 +79,20 @@ class Graph {
     });
   }
 
+  Set<String> dfsTraversal(Graph graph, String root) {
+    Set<String> visited = new LinkedHashSet<String>();
+    Stack<String> stack = new Stack<String>();
+    stack.push(root);
+    while (!stack.isEmpty()) {
+      String vertex = stack.pop();
+      if (!visited.contains(vertex)) {
+        visited.add(vertex);
+        for (Vertex v: graph.getAdjVertices(vertex)) {
+          stack.push(v.label);
+        }
+      }
+    }
+    return visited;
+  }
+
 }
