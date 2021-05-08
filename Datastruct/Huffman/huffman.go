43d56e5327a569d967b337d054c05aa5009c67b8
Muaz Wazir <muazwazir@gmail.com>
2021-05-09 00:32:20 +0800

    methods for hufmann pt 2

--- Datastruct/Huffman/huffman.go
+++ Datastruct/Huffman/huffman.go
@@ -1,5 +1,8 @@
 package main
 
+import "container/heap"
+
+type treeHeap []HuffmanTree
 type HuffmanTree interface {
 	Frequency() int
 }
@@ -20,4 +23,30 @@ func (self HuffmanLeaf) Frequency() int {
 
 func (self HuffmanNode) Frequency() int {
 	return self.freq
+}
+
+func (th treeHeap) Lens() int { return len(th) }
+func (th treeHeap) Less(i, j int) bool {
+	return th[i].Frequency() < th[j].Frequency()
+}
+
+func (th *treeHeap) Push(element interface{}) {
+	*th = append(*th, element.(HuffmanTree))
+}
+
+func (th *treeHeap) Pop(popped interface{}) {
+	popped = (*th)[len(*th)-1]
+	*th = (*th)[:len(*th)-1]
+	return
+}
+
+func (th treeHeap) Swap(i, j int) { th[i], th[j] = th[j], th[i] }
+
+func buildTree(symFreqs map[rune]int) HuffmanTree {
+	var trees treeHeap
+
+	for c, f := range symFreqs {
+		trees = append(trees, HuffmanLeaf{f, c})
+	}
+	heap.Init(&trees)
 }
