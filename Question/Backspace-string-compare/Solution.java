76b5a037287c6f95506a9817899c18c7e5331575
muazwzxv <muazwazir@gmail.com>
2022-06-13 22:36:39 +0800

    backspace String Compare java O(n) solution

---
+++ Question/Backspace-string-compare/Solution.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        
+        List valueS = checkString(s);
+        List valueT = checkString(t);
+        
+        if (valueS.size() != valueT.size())
+            return false;
+        
+        return valueS.equals(valueT);
+        
+    }
+    
+    public List checkString(String str) {
+        LinkedList<Character> list = new LinkedList<>();
+        
+        for (int i = 0; i < str.length(); i ++) {
+            Character temp = str.charAt(i);
+            
+            if (temp != '#') {
+                list.add(temp);
+            } 
+            else {
+                if (!list.isEmpty()) {
+                    list.removeLast();
+                }
+            }
+        }
+        
+        return list;
+    }
+}
