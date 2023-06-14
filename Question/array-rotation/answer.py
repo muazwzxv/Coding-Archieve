df414344d9a292115cbdecb021d575edb552759e
Muaz Wazir <muazwazir@gmail.com>
2023-06-14 23:27:21 +0800

    source code

---
+++ Question/array-rotation/answer.py
@@ -0,0 +1,11 @@
+
+def rotLeft(arr, n):
+  if not arr:
+      return arr
+  
+  length = len(arr)
+  rotated_array = []
+  for i in range(n, n + length):
+      rotated_array.append(arr[i % length])
+
+  return rotated_array`
